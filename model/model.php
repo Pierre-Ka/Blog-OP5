4de3ac02e581368a7926e2bde7718d62613439fe
Pierre Ka <ikanhiu@outlook.fr>
2021-12-18 09:11:56 +0100

    creation du dossier vide

---
+++ model/model.php
@@ -0,0 +1,15 @@
+<?php
+
+/* LISTE DES FONCTIONS PHP : 
+
+FONCTION DE CONNEXION
+-isUnique
+-isPasswordCorrect
+
+FONCTIONS DE CREATION POST
+- savePost
+- updatePost
+- deletePost
+FONCTION DE CREATION COM
+- saveCom
+- updateCom
